2cdfd2927a4131c7497fac3e544c3f727190ae6e
zhuobenj <benjamin.zhuo@mail.utoronto.ca>
2019-10-15 23:22:32 -0400

    Fixed part 2

--- Lab5/divider.v
+++ Lab5/divider.v
@@ -1,37 +1,36 @@
 module divider(CLOCK_50, SW, KEY, HEX0);
+    input[1:0] SW;
     input CLOCK_50;
     input[1:0] KEY;
-    input[1:0] SW;
     output[6:0] HEX0;
-    wire[31:0] rateddivider_out;
+
+    wire rateddivider_out;
     wire[3:0] displayCounter_out;
-    wire enable;
-    reg [31:0] period;
+    reg [27:0] period;
 
-    assign enable = (rateddivider_out == 0) ? 1 : 0;
-
-    always @(SW)
+    // Speed
+    always @(*)
     begin
         case(SW[1:0])
-            2'b00: period <= 1;
-            2'b01: period <= 50000000;
-            2'b10: period <= 100000000;
-            2'b11: period <= 200000000;
+            2'b00: period = 0;
+            2'b01: period = 28'd49999999;
+            2'b10: period = 28'd99999999;
+            2'b11: period = 28'd199999999;
             default: period <= 0;
         endcase
     end
 
     rateddivider u0(
-        .clock(CLOCK_50),
+        .CLOCK_50(CLOCK_50),
         .reset_n(KEY[0]),
         .period(period),
-        .q(rateddivider_out)
+        .cout(rateddivider_out)
     );
 
     displayCounter u1(
-        .clock(CLOCK_50),
+        .CLOCK_50(CLOCK_50),
         .reset_n(KEY[0]),
-        .enable(enable),
+        .enable(rateddivider_out),
         .q(displayCounter_out)
     );
 
@@ -41,89 +40,62 @@ module divider(CLOCK_50, SW, KEY, HEX0);
     );
 endmodule
 
-module counter(clock, reset_n, reload, display, select);
-    output[3:0] display;
-    input clock, reset_n, reload;
-    input[1:0] select;
-    wire[27:0] connection0, connection1;
+module rateddivider(period, reset_n, CLOCK_50, cout);
+    input CLOCK_50, reset_n;
+    input[27:0] period;
+    output cout;
 
-endmodule
+    reg[27:0] count;
 
-module rateddivider(clock, period, reset_n, q);
-    input clock, reset_n;
-    input[31:0] period;
-    output reg [31:0] q;
-
-    always @(posedge clock)
+    always @(posedge CLOCK_50)
     begin
         if (reset_n == 1'b0)
-            q <= period - 1;
+            count <= period;
         else
             begin 
-                if (q == 0)
-                    q <= period -1;
+                if (count == 0)
+                    count <= period;
                 else
-                    q <= q - 1'b1;
+                    count <= count - 1'b1;
             end
     end
+    assign q = (count == 0) ? 1 : 0;
 endmodule
 
-module displayCounter(clock, reset_n, enable, q);
-    input clock, reset_n, enable;
+module displayCounter(CLOCK_50, reset_n, enable, q);
+    input CLOCK_50, reset_n, enable;
     output reg [3:0] q;
     
-    always @(posedge clock)
+    always @(posedge CLOCK_50)
     begin
         if(reset_n == 1'b0)
             q <= 0;
         else if (enable == 1'b1)
-            begin
-                if(q == 4'b1111)
-                    q <= 0;
-                else
-                    q <= q + 1'b1;
-            end
+            q <= q + 1'b1;
     end
 endmodule
 
-module mux4to1(out,u,v,w,x,switch);
-    input [1:0]switch;
-    input [27:0]u;
-    input [27:0]v;
-    input [27:0]w;
-    input [27:0]x;
-    output [27:0]out;
-    reg [27:0] result;
-
-    assign out = result;
-
-    always @(*)
-    begin
-        case(switch)
-            2'b00: //u
-                result = u;
-            2'b01: //v
-                result = v;
-            2'b10: //w
-                result = w;
-            2'b11: //x
-                result = x;
-        endcase
-    end
-endmodule
-
-module HEX(S,H);
-    input [3:0]S;
-    output [6:0]H;
-   
-    assign H[0]=(~S[3]&~S[2]&~S[1]&S[0]) |
-                (~S[3]&S[2]&~S[1]&~S[0])|
-                (S[3]&~S[2]&S[1]&S[0])|
-                (S[3]&S[2]&~S[1]&S[0]);
-    assign H[1]=(~S[3]&S[2]&~S[1]&S[0])|(S[3]&S[1]&S[0])|(S[2]&S[1]&~S[0])|(S[3]&S[2]&~S[0]);
-    assign H[2]=(~S[3]&~S[2]&S[1]&~S[0])|(S[3]&S[2]&S[1])|(S[3]&S[2]&~S[0]);
-    assign H[3]=(~S[3]&S[2]&~S[1]&~S[0])|(S[3]&~S[2]&S[1]&~S[0])|(S[2]&S[1]&S[0])|(~S[2]&~S[1]&S[0]);
-    assign H[4]=(~S[3]&S[0])|(~S[2]&~S[1]&S[0])|(~S[3]&S[2]&~S[1]);
-    assign H[5]=(S[3]&S[2]&~S[1]&S[0])|(~S[3]&S[1]&S[0])|(~S[3]&~S[2]&S[0])|(~S[3]&~S[2]&S[1]);
-    assign H[6]=(S[3]&S[2]&~S[1]&~S[0])|(~S[3]&S[2]&S[1]&S[0])|(~S[3]&~S[2]&~S[1]);
-endmodule
+module SevenSegmentDecoder(in, out);
+	input[3:0]in;
+	output reg [6:0]out;
+	
+	always @*
+		case(in)
+			4'b0000 : out = ~7'b0111111; //0
+			4'b0001 : out = ~7'b0000110; //1
+			4'b0010 : out = ~7'b1011011; //2
+			4'b0011 : out = ~7'b1001111; //3
+			4'b0100 : out = ~7'b1100110; //4
+			4'b0101 : out = ~7'b1101101; //5 
+			4'b0110 : out = ~7'b1111101; //6
+			4'b0111 : out = ~7'b0000111; //7
+			4'b1000 : out = ~7'b1111111; //8
+			4'b1001 : out = ~7'b1101111; //9
+			4'b1010 : out = ~7'b1110111; //A
+			4'b1011 : out = ~7'b1111100; //b
+			4'b1100 : out = ~7'b0111001; //C
+			4'b1101 : out = ~7'b1011110; //d
+			4'b1110 : out = ~7'b1111001; //E
+			4'b1111 : out = ~7'b1110001; //F
+		endcase
+endmodule 
