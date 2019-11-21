6bfd68e711681a01b6d090ce1f2db9aecf43e1a6
ben78912 <43190469+ben78912@users.noreply.github.com>
2019-11-21 00:56:45 -0500

    Created loop to move the car on screen

--- FinalProject/game.v
+++ FinalProject/game.v
@@ -35,15 +35,24 @@ module game(
 	output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
 	output  [6:0]  HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7;
 	
+	
+	localparam none = 2'b00,
+				  left = 2'b01,
+				  right = 2'b10,
+				  speed = 10;
+	
 	wire resetn;
 	assign resetn = KEY[0];
 	wire [2:0] colour;
-	wire [7:0] x;
-	wire [7:0] y;
+	reg [7:0] x_init = 8'b10100000;
+	wire [7:0] y_init = 8'b11111111;
+	wire [7:0] x_final;
+	wire [7:0] y_final;
    wire [7:0] keyValue;
    wire tick;
    reg [31:0] tps = 32'd15;
 	wire writeEn, enable, id_x, id_y;
+	reg [2:0] direction = none;
 
     // Create an Instance of a VGA controller - there can be only one!
 	// Define the number of colours as well as the initial background
@@ -52,8 +61,8 @@ module game(
 			.resetn(resetn),
 			.clock(CLOCK_50),
 			.colour(colour),
-			.x(x),
-			.y(y),
+			.x(x_final),
+			.y(y_final),
 			.plot(writeEn),
 			/* Signals for the DAC to drive the monitor. */
 			.VGA_R(VGA_R),
@@ -69,35 +78,67 @@ module game(
 		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
 		defparam VGA.BACKGROUND_IMAGE = "black.mif";
 
-    // always @(posedge tick) begin
-    //     if (keyValue == )
-
     keyboardController(CLOCK_50, PS2_DAT, PS2_CLK, keyValue);
+	 Tick(CLOCK_50, tick, tps);
+	 
+//	 always @(posedge tick) begin
+//		if (keyValue == 116 & direction != left) begin // Right if not left
+//				direction <= right;
+//		end else if (keyvalue == 107 & direction != right) begin // left if not right
+//				direction <= left;
+//		end else (direction != left & direction != right & direction != 2'b11) begin // No movement
+//				direction <= none;
+//		end
+//	 
+//		if (direction = left) begin // Left
+//			x_init <= x_init - speed;
+//		end else if (direction = right) // Right
+//			x_init <= x_init + speed;
+//		// No movement
+//		end
+//	end
+	
+	always @(posedge tick) begin
+		if (SW[1] & direction != left) begin // left if not right 
+				direction <= left;
+		end else if (SW[0] & direction != right) begin // Right if not left
+				direction <= right;
+		end else begin // No movement
+				direction <= none;
+		end
+
+		if (direction == left) begin // Left
+			x_init <= x_init - speed;
+		end else if (direction == right) begin// Right
+			x_init <= x_init + speed;
+		// No movement
+		end
+	end
 	 
 	 sprite_ram #(
         .WIDTH_X(5),
-        .WIDTH_Y(5),
+        .WIDTH_Y(7),
         .RESOLUTION_X(27),
         .RESOLUTION_Y(48),
         .MIF_FILE("PixelCar.mif")
     ) srm_frog (
         .clk(CLOCK_50),
-        .x(x), .y(y),
+        .x(x_final), .y(y_final),
         .color_out(colour)
     );
 	 
 	// Instansiate datapath
 	// datapath d0(...);
 	datapath d0(
-		.x_in(8'b10100000),
-		.y_in(8'b11111111),
+		.x_in(x_init),
+		.y_in(y_init),
 		.clock(CLOCK_50),
 		.resetn(resetn),
 		.controlX(id_x),
 		.controlY(id_y),
 		.enable_x(enable),
-		.x_out(x),
-		.y_out(y)
+		.x_out(x_final),
+		.y_out(y_final)
 	);
 	
     // Instansiate FSM control
