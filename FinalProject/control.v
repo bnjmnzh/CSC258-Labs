ac5821dff1433d82de2584b6d41bd5af0cc795b2
ben78912 <benjamin.zhuo@mail.utoronto.ca>
2019-11-25 23:30:43 -0500

    Fixed typo

--- FinalProject/control.v
+++ FinalProject/control.v
@@ -16,6 +16,7 @@ module control(clock, reset, done, erase, want_move, en_vga, en_datapath, can_mo
         else
             curr <= next; 
     end
+
     // State Table
     always @(*)
     begin: state_table
@@ -23,7 +24,7 @@ module control(clock, reset, done, erase, want_move, en_vga, en_datapath, can_mo
 			PLOT: next = done ? PLOT_WAIT : PLOT ;
             PLOT_WAIT: next = want_move ? ERASE : PLOT_WAIT ;
             ERASE: next = done ? PLOT : ERASE ;
-				MOVE: next = PLOT;
+			MOVE: next = PLOT;
             default: next = PLOT;
         endcase
     end
@@ -32,18 +33,18 @@ module control(clock, reset, done, erase, want_move, en_vga, en_datapath, can_mo
     begin
         case (curr)
             PLOT:
-	         begin
-				en_vga = 1;
-				en_datapath = 1;
+	        begin
+			en_vga = 1;
+			en_datapath = 1;
             erase = 0;
-				can_move = 0;
-				end
+			can_move = 0;
+			end
             PLOT_WAIT:
             begin
             en_vga = 0;
             en_datapath = 0;
             erase = 0;
-				can_move = 0;	 
+			can_move = 0;	 
             end
             ERASE:
             begin
@@ -51,8 +52,8 @@ module control(clock, reset, done, erase, want_move, en_vga, en_datapath, can_mo
             en_datapath = 1;
             erase = 1;
             can_move = 0;
-				end
-				MOVE: can_move = 1;
+			end
+			MOVE: can_move = 1;
         endcase
     end
 endmodule
--- FinalProject/datapath.v
+++ FinalProject/datapath.v
@@ -10,35 +10,71 @@ module datapath(x_in, y_in, clock, resetn, done, enable, x_out, y_out);
 
 	reg [6:0] i_x;
 	reg [7:0] i_y;
+    reg enable_y;
 
-	always @ (posedge clock) begin
+	// always @ (posedge clock) begin
 	
-			// Reset when no enable signal.
-        if (!enable || resetn) begin
-             i_x <= 0;
-             i_y <= 0;
-        end
+	// 		// Reset when no enable signal.
+    //     if (!enable || resetn) begin
+    //          i_x <= 0;
+    //          i_y <= 0;
+    //     end
        
-        // Increment on enable signal.
-        if (enable) begin
-            if (i_x < 27) begin
-                i_x <= i_x + 1;
-            end else begin
+    //     // Increment on enable signal.
+    //     if (enable) begin
+    //         if (i_x < 27) begin
+    //             i_x <= i_x + 1;
+    //         end else begin
+    //             i_x <= 0;
+    //             i_y <= i_y + 1;
+    //         end
+    //     end
+ 
+    //     // Signal done when reached max.
+    //     // MAX_X - 2 is so that the signal is high during the last (x, y).
+    //     if (i_x == 27 && i_y == 48) begin
+    //         done <= 1;
+    //     end else begin
+    //         done <= 0;
+    //     end
+ 
+    // 
+    
+    always @(posedge clock) begin
+        if (!enable || resetn) begin
+            i_x <= 0;
+            i_y <= 0;
+        end
+    end
+
+    always @(posedge clock) begin
+        if (!resetn) 
+            i_x <= 0;
+        else if (enable) begin
+            if (i_x == 26) begin
                 i_x <= 0;
-                i_y <= i_y + 1;
+                enable_y <= 1;
+            end else begin
+                i_x <= i_x + 1;
+                i_y <= 0;
             end
         end
- 
-        // Signal done when reached max.
-        // MAX_X - 2 is so that the signal is high during the last (x, y).
-        if (i_x == 27 && i_y == 48) begin
-            done <= 1;
-        end else begin
-            done <= 0;
-        end
- 
     end
-			
+
+    always @(posedge clock) begin
+		if (!resetn)
+			i_y <= 0;
+		else if(enable_y) begin
+			if(i_y == 47) begin
+                i_y <= 0;
+                if (i_x == 26)
+                    done <= 1;
+                else 
+                    done <= 0;
+			end else 
+				i_y <= i_y + 1;
+		    end
+	end
 	
 	assign x_out = x_in + i_x;
 	assign y_out = y_in + i_y;
