be2ec1a01598389e17b2c0e1a3da518d145359a8
ben78912 <benjamin.zhuo@mail.utoronto.ca>
2019-11-30 17:55:32 -0500

    Added pedestrian image

--- FinalProject/fibonacci_lfsr_7bit.v
+++ FinalProject/fibonacci_lfsr_7bit.v
@@ -2,14 +2,18 @@ module fibonacci_lfsr_7bit(
   input clk,
   input rst_n,
 
-  output reg [6:0] data
+  output reg [8:0] data
 );
 
-reg [6:0] data_next;
+reg [8:0] data_next;
 
 always @(*) begin
-  data_next[4] = data[4]^data[1];
-  data_next[3] = data[3]^data[0];
+  data_next[8] = data[8]^data[1];
+  data_next[7] = data[7]^data[0];
+  data_next[6] = data[6]^data[8];
+  data_next[5] = data[5]^data[7];
+  data_next[4] = data[4]^data[6];
+  data_next[3] = data[3]^data[5];
   data_next[2] = data[2]^data_next[4];
   data_next[1] = data[1]^data_next[3];
   data_next[0] = data[0]^data_next[2];
@@ -17,7 +21,7 @@ end
 
 always @(posedge clk or negedge rst_n)
   if(!rst_n)
-    data <= 5'h1f;
+    data <= 5'h1f;  
   else
     data <= data_next;
 
